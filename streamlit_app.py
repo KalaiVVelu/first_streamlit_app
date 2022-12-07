fcdfe596b3addb5289385f16a99848099c4586f3
KalaiVVelu <64990613+KalaiVVelu@users.noreply.github.com>
2022-12-07 18:30:57 +0000

    Update streamlit_app.py
    Move all the snowflake related statements to a function block and commented the rest of the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,26 +58,40 @@ except URLError as e:
 #Do not run anything beyound this point
 streamlit.stop()
 
+streamlit.header("The fruit load contains")
+#Snowflake related functions
+def get_fruit_load_list():
+        with my_cnx.cursor() as my_cur:
+             my_cur.execute("select * from fruit_load_list")   
+        return my_cur.fetchall()
+
+#Adda button to load the fruit
+if streamlit.button('Get Fruit Load list')
+        my_cnx = my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = get_fruit_load_list()
+        streamlit.dataframe(my_data_rows)
+
+
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-my_cur.execute("SELECT * from fruit_load_list")
+#my_cur.execute("SELECT * from fruit_load_list")
 #Fetch a single row
 #my_data_row = my_cur.fetchone()
 
 #Lets fetch all the rows from the table
-my_data_rows = my_cur.fetchall()
+#my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 
-streamlit.text("The fruit load list contains:")
+#streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
-streamlit.dataframe(my_data_rows)
+#streamlit.dataframe(my_data_rows)
 
 #Allow the user to add a fruit
-add_my_fruit = streamlit.text_input('What fruit would you like to add ','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#add_my_fruit = streamlit.text_input('What fruit would you like to add ','Jackfruit')
+#streamlit.write('Thanks for adding ', add_my_fruit)
 
 #Add execution statement to connect to Snowflake
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
