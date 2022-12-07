c90d836459f8294cdbc19bb056f04c08f391a102
KalaiVVelu <64990613+KalaiVVelu@users.noreply.github.com>
2022-12-07 17:52:16 +0000

    Update streamlit_app.py
    Fetch all the rows instead and display the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,16 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+#Fetch a single row
+#my_data_row = my_cur.fetchone()
+
+#Lets fetch all the rows from the table
+my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
+
+
 streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
