a3c97bd7efa8f9f8f0b6f98b111ee159868cbd78
KalaiVVelu <64990613+KalaiVVelu@users.noreply.github.com>
2022-12-07 18:15:53 +0000

    Update streamlit_app.py
    Adding a new try catch block

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,23 +24,29 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header("FruityVice Fruit Advice")
 #Commenting the Json response
 #streamlit.text(fruityvice_response.json())
 
 #New Section to display on the FruityVice API Response
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.header("FruityVice Fruit Advice")
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else     
+        #Adding Kiwi fruit, adding the dynamic variable 
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        #Take the JSON object and normalize it
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        #Output to the screen as a table
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+
+#streamlit.write('The user entered ', fruit_choice)
 
 #import requests
-#Adding Kiwi fruit, adding the dynamic variable 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-#Take the JSON object and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#Output to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 #Do not run anything beyound this point
 streamlit.stop()
