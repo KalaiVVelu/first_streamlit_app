9c05bbe8b1e1687587f5f940ffe1ef9a212161cb
KalaiVVelu <64990613+KalaiVVelu@users.noreply.github.com>
2022-12-07 18:16:35 +0000

    Update streamlit_app.py
    Corrected the syntax error line 36

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information")
-    else     
+    else:     
         #Adding Kiwi fruit, adding the dynamic variable 
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         #Take the JSON object and normalize it
